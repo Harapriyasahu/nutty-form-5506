ec714d4683c59f7545e071011508bac39a756d62
deepRaj06git config --global user.name deepRaj06 <deepak.onlinework13@gmail.com>
2022-09-28 22:11:23 +0530

    worked on projects, searchbar, tasklist and navbar

--- frontend/src/Components/Project_Sections/Searchbar.jsx
+++ frontend/src/Components/Project_Sections/Searchbar.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import { Box, Flex, Icon, InputGroup, InputLeftAddon } from "@chakra-ui/react";
+import React from "react";
+import { Input } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Select } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
+import { BiCollapse } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+import { FiFilter } from "react-icons/fi";
 
 const Searchbar = () => {
   return (
-    <div>
-        Searchbar
-    </div>
-  )
-}
+    <Box
+      w="1275px"
+      h="94px"
+      // border="1px solid red"
+    >
+      <Box
+        w="675px"
+        h="94px"
+        // border="1px solid red"
+        p="5"
+      >
+        <Flex flexDirection="row" justifyContent="space-evenly">
+          <Input
+            bg="whitesmoke"
+            focusBorderColor="lightgreen"
+            htmlSize={14}
+            width="auto"
+            placeholder="Search"
+          />
+          <Button cursor="pointer" colorScheme="whatsapp">
+            Add Project
+          </Button>
+          {/* <InputGroup> */}
+          {/* <InputLeftAddon w='2' bg='white' children={<Icon as={FiFilter} />} /> */}
 
-export default Searchbar
+          <Select cursor="pointer" placeholder="Filter" w="26">
+            <option value="option1">All Tasks</option>
+            <option value="option2">My Tasks</option>
+          </Select>
+          {/* </InputGroup> */}
+
+          <IconButton
+            cursor="pointer"
+            icon={<Icon as={BiCollapse} />}
+            // as={BiCollapse}
+            variant="outline"
+            color="gray"
+            // colorScheme="gray"
+            aria-label="Call Sage"
+            fontSize="20px"
+            // icon={BiCollapse}
+          />
+          <Button cursor="pointer" color="gray" variant="outline">
+            Archived
+          </Button>
+          <IconButton
+            cursor="pointer"
+            // as={BsThreeDots}
+            icon={<Icon as={BsThreeDots} />}
+            variant="outline"
+            color="gray"
+            // aria-label="Call Sage"
+            // fontSize="12px"
+            w="10"
+            h="10"
+            // icon={BsThreeDots}
+          />
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Searchbar;
--- frontend/src/Components/Project_Sections/TaskList.jsx
+++ frontend/src/Components/Project_Sections/TaskList.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
 
 const TaskList = () => {
   return (
-    <div>
-        TaskList
-    </div>
-  )
-}
+    <Box w="1275px" h="600px">
+      <Box
+        boxSizing="border-box"
+        w="1275px"
+        h="580px"
+        // border="1px solid black"
+      >
+        <Flex
+          flexDirection="row"
+          // border="1px solid green"
+          justifyContent="space-around"
+        >
+          {/* Left Side TaskList Section */}
+          <Box
+            w="700px"
+            h="108px"
+            // border="1px solid blue"
+            p="2"
+          >
+            <Box
+              color="lightgray"
+              borderRadius="4px"
+              ml="1.2rem"
+              w="620px"
+              h="100px"
+              border="1px solid lightgrey"
+            ></Box>
+          </Box>
 
-export default TaskList
+          {/* Right Side Add/Edit Section */}
+          <Box
+            w="490px"
+            h="463px"
+            // border="1px solid red"
+            mr="6rem"
+            p="2"
+          >
+            <Box
+              color="lightgray"
+              borderRadius="4px"
+              ml="1rem"
+              w="453px"
+              h="100px"
+              border="1px solid lightgrey"
+            >
+              <Text mt="2rem" color="lightgray">
+                Choose project or task to edit
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default TaskList;
