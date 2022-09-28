93a34feb256dc4a4e4377f8b2864d6e352e7fded
gkomsai <gkomsai788@gmail.com>
2022-09-28 14:09:27 +0530

    backend login and signup authentication added

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+module.exports = { UserModel };
