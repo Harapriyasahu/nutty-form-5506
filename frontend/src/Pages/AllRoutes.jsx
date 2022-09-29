ffa8fa8e6db67cb06a44ec413ccbf314cedadefd
unnikrishnansk <unni.sk94@gmail.com>
2022-09-29 21:45:13 +0530

    first commit

--- frontend/src/Pages/AllRoutes.jsx
+++ frontend/src/Pages/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Projects from "./Projects";
 import Home from './Home'
+import Timesheet from "./Timesheet";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
+      {/* <Route path="/projects" element={<Projects />} /> */}
+      <Route path="/timesheet" element={<Timesheet />} />
     </Routes>
   );
 };
