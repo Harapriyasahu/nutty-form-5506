999f260db3531ce77c4236f0ce315c4062583454
gkomsai <gkomsai788@gmail.com>
2022-09-29 17:09:09 +0530

    Login Component Created

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route,} from "react-router-dom";
 import Projects from "../Pages/Projects";
 import Home from '../Pages/Home'
 import Signup from "../Pages/Signup/Signup";
+import Login from "../Pages/Login/Login";
 
 const AllRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AllRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
