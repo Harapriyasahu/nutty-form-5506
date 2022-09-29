3178bc8715bbef29fec123083f4853384aff7a5a
gkomsai <gkomsai788@gmail.com>
2022-09-29 18:27:14 +0530

    LoginPage left section created

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,76 +1,13 @@
-import React from "react";
-import { Box, Button, Input, Text } from "@chakra-ui/react";
-import styles from "./Login.module.css";
-import { FcGoogle } from "react-icons/fc";
-import { Link, useNavigate } from "react-router-dom";
+import { Flex } from "@chakra-ui/react";
+import LeftSection from "./LeftSection";
+import RightSectiion from "./RightSectiion";
 
 const Login = () => {
-  const navigate = useNavigate();
-
   return (
-    <Box className={styles.logindiv}>
-      <Text className={styles.loginheading}>Log in to TimeCamp</Text>
-
-      <Box
-        className={styles.googlebtn}
-        _hover={{ backgroundColor: "gray.100" }}
-      >
-        <FcGoogle fontSize="30px" />
-        <Text fontSize="14px" fontWeight="700" color="#8f7e77">
-          Log in with Google
-        </Text>
-      </Box>
-
-      <Text marginTop="20px" fontSize="14px">
-        Or
-      </Text>
-
-      <Box margin="auto" width="75%" marginTop="20px">
-        <Input focusBorderColor="#25cf60" placeholder="email" type="email" />
-        <Input
-          type="password"
-          focusBorderColor="#25cf60"
-          placeholder="password"
-          marginTop="15px"
-        />
-      </Box>
-
-      <Link to="/#">
-        <Text color="#25cf60" marginTop="15px" fontSize="14px" cursor="pointer">
-          Forgotten Password?
-        </Text>
-      </Link>
-
-      <Button
-        className={styles.loginbtn}
-        backgroundColor="#25cf60"
-        cursor="pointer"
-        _hover={{ backgroundColor: "#25cf60" }}
-      >
-        Log in
-      </Button>
-
-      <Text
-        color="gray"
-        fontSize="14px"
-        width="65%"
-        margin="auto"
-        marginTop="20px"
-        textAlign="center"
-      >
-        <span
-          onClick={() => navigate("/signup")}
-          style={{ color: "#25cf60", cursor: "pointer" }}
-        >
-          No account? Sign up
-        </span>{" "}
-        or
-        <span style={{ color: "#25cf60", cursor: "pointer" }}>
-          {" "}
-          Log in with SSO
-        </span>
-      </Text>
-    </Box>
+    <Flex width={"80%"} justify="space-between" border={"1px"} m="auto" mt="5rem" >
+      <LeftSection />
+      <RightSectiion />
+    </Flex>
   );
 };
 
