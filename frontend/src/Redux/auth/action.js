d31d2b92c815574ac2dd0d16b53caac3d37dccc3
gkomsai <gkomsai788@gmail.com>
2022-09-29 14:26:01 +0530

    Redux updated

--- frontend/src/Redux/auth/action.js
+++ frontend/src/Redux/auth/action.js
@@ -1,47 +1,46 @@
 import {
-    USER_SIGNUP_REQUEST,
-    USER_SIGNUP_SUCCESS,
-    USER_SIGNUP_FAILURE,
-    USER_LOGIN_REQUEST,
-    USER_LOGIN_SUCCESS,
-    USER_LOGIN_FAILURE,
-  } from "./actionTypes";
-  
-  export const signupRequest = () => {
-    return {
-      type: USER_SIGNUP_REQUEST,
-    };
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
+  USER_SIGNUP_FAILURE,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAILURE,
+} from "./actionTypes";
+
+export const signupRequest = () => {
+  return {
+    type: USER_SIGNUP_REQUEST,
   };
-  
-  export const signupSuccess = (payload) => {
-    return {
-      type: USER_SIGNUP_SUCCESS,
-      payload,
-    };
+};
+
+export const signupSuccess = (payload) => {
+  return {
+    type: USER_SIGNUP_SUCCESS,
+    payload,
   };
-  
-  export const signupFailure = () => {
-    return {
-      type: USER_SIGNUP_FAILURE,
-    };
+};
+
+export const signupFailure = () => {
+  return {
+    type: USER_SIGNUP_FAILURE,
   };
-  
-  export const loginRequest = () => {
-    return {
-      type: USER_LOGIN_REQUEST,
-    };
+};
+
+export const loginRequest = () => {
+  return {
+    type: USER_LOGIN_REQUEST,
   };
-  
-  export const loginSuccess = (payload) => {
-    return {
-      type: USER_LOGIN_SUCCESS,
-      payload,
-    };
+};
+
+export const loginSuccess = (payload) => {
+  return {
+    type: USER_LOGIN_SUCCESS,
+    payload,
   };
-  
-  export const loginFailure = () => {
-    return {
-      type: USER_LOGIN_FAILURE,
-    };
+};
+
+export const loginFailure = () => {
+  return {
+    type: USER_LOGIN_FAILURE,
   };
-  
+};
