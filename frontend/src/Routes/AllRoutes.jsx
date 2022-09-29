f6cedc416d0d3bfcb62061e9d0332f30f2b98f48
gkomsai <gkomsai788@gmail.com>
2022-09-29 15:42:06 +0530

    Signup page added

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route,} from "react-router-dom";
 import Projects from "../Pages/Projects";
 import Home from '../Pages/Home'
+import Signup from "../Pages/Signup/Signup";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
