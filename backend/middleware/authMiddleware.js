0e65766df2887d90f807cb35dfa0dde1a4c7dea8
gkomsai <gkomsai788@gmail.com>
2022-09-30 18:45:40 +0530

    auth-middleware updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 
 const checkUserAuth = async (req, res, next) => {
   if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer")) {
-   return res.status(401).send("Please login first");
+   return res.status(401).send({message:"Please login first"});
   }
 
   const token = req.headers.authorization.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
       if (err) {
-        res.status(401).send("Something went wrong, please login again");
+       return res.status(401).send({message:"Something went wrong, please login again"});
       } else {
         req.body.userId = decoded.userId; 
         req.body.email = decoded.email
