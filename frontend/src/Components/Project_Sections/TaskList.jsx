1bad8bac262345f30749588bb3c4ae72fe3fec8f
deepRaj06git config --global user.name deepRaj06 <deepak.onlinework13@gmail.com>
2022-09-30 12:57:14 +0530

    TaskList almost completed

--- frontend/src/Components/Project_Sections/TaskList.jsx
+++ frontend/src/Components/Project_Sections/TaskList.jsx
@@ -33,13 +33,36 @@ const TaskList = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [textBox, setTextBox] = useState(false);
+  const [editBox, setEditBox] = useState(false);
+  const [closeTextBox, setCloseTextBox] = useState(false);
+  const [closeEditBox, setCloseEditBox] = useState(false);
 
   const handleClose = () => {
-    setTextBox(true);
+    setTextBox(false);
+    setCloseTextBox(false);
   };
 
   const handleShowTask = () => {
-    setTextBox(false);
+    if (editBox === true) {
+      setEditBox(false);
+    }
+    setCloseEditBox(false);
+    setCloseTextBox(true);
+    setTextBox(true);
+  };
+
+  const handleEditClose = () => {
+    setEditBox(false);
+    setCloseEditBox(true);
+  };
+
+  const handleEditShowTask = () => {
+    if (textBox === true) {
+      setTextBox(false);
+    }
+    setCloseEditBox(false);
+    setCloseTextBox(true);
+    setEditBox(true);
   };
 
   return (
@@ -109,10 +132,11 @@ const TaskList = () => {
                               flexDirection="row"
                               justifyContent="space-evenly"
                             >
-                              <Button 
-                              cursor="pointer"
-                              onClick={handleShowTask} 
-                              colorScheme="whatsapp">
+                              <Button
+                                cursor="pointer"
+                                onClick={handleShowTask}
+                                colorScheme="whatsapp"
+                              >
                                 Add task
                               </Button>{" "}
                               <IconButton
@@ -132,6 +156,7 @@ const TaskList = () => {
                                 stroke="grey"
                               />
                               <IconButton
+                                onClick={handleEditShowTask}
                                 cursor="pointer"
                                 icon={<Icon as={BsThreeDots} />}
                                 variant="outline"
@@ -202,10 +227,11 @@ const TaskList = () => {
                               flexDirection="row"
                               justifyContent="space-evenly"
                             >
-                              <Button 
-                              cursor="pointer" 
-                              onClick={handleShowTask}
-                              colorScheme="whatsapp">
+                              <Button
+                                cursor="pointer"
+                                onClick={handleShowTask}
+                                colorScheme="whatsapp"
+                              >
                                 Add task
                               </Button>{" "}
                               <IconButton
@@ -274,25 +300,39 @@ const TaskList = () => {
               overflow="scroll"
               overflowX="hidden"
             >
-              {textBox === true ? (
-                <Box scrollBehavior='none' h='100px'>
-                <Text mt="2rem" color="lightgray">
-                  Choose project or task to edit
-                </Text>
+              {closeTextBox === false ? (
+                <Box scrollBehavior="none" h="100px">
+                  <Text mt="2rem" color="lightgray">
+                    Choose project or task to edit
+                  </Text>
                 </Box>
               ) : (
-                <Box
-                  w="453px"
-                  // h="200px"
-                >
-                  <Box
-                    w="446px"
-                    // h="20px"
-                  >
+                ""
+              )}
+              {closeEditBox === true ? (
+                <Box scrollBehavior="none" h="100px">
+                  <Text mt="2rem" color="lightgray">
+                    Choose project or task to edit
+                  </Text>
+                </Box>
+              ) : (
+                ""
+              )}
+
+              {textBox === false ? (
+                ""
+              ) : (
+                <Box w="453px">
+                  <Box w="446px" mb="0.8rem">
                     <Flex justifyContent="space-between">
                       <Text>Add Task</Text>
-                      {/* <CloseButton onClick={() => handleClose} /> */}
-                      <Button onClick={handleClose}>X</Button>
+                      <Button
+                        height="0"
+                        variant="unstyled"
+                        onClick={handleClose}
+                      >
+                        X
+                      </Button>
                     </Flex>
                   </Box>
 
@@ -384,6 +424,159 @@ const TaskList = () => {
                     <Flex flexDirection="row" alignItems="flex-start">
                       <Button colorScheme="whatsapp" variant="solid">
                         Add Task
+                      </Button>
+                      <Button ml="0.8rem" colorScheme="grey" variant="outline">
+                        Cancel
+                      </Button>
+                    </Flex>
+                  </Box>
+                </Box>
+              )}
+
+              {editBox === false ? (
+                ""
+              ) : (
+                <Box
+                  w="453px"
+                  // h="200px"
+                >
+                  <Box
+                    w="446px"
+                    mb="0.8rem"
+                    // h="20px"
+                  >
+                    <Flex justifyContent="space-between">
+                      <Text>Edit Task</Text>
+                      {/* <CloseButton onClick={() => handleClose} /> */}
+                      <Button
+                        height="0"
+                        variant="unstyled"
+                        onClick={handleEditClose}
+                      >
+                        X
+                      </Button>
+                    </Flex>
+                  </Box>
+
+                  <Box w="446px" mb="0.8rem">
+                    <FormControl>
+                      <FormLabel>First Project/</FormLabel>
+                      <Input type="Task name..." />
+                    </FormControl>
+                  </Box>
+
+                  <Box w="446px" mb="0.8rem">
+                    <Flex justifyContent="space-between">
+                      <Text mt="0.5rem" w="50px">
+                        People
+                      </Text>
+
+                      <Box w="280px">
+                        <Flex justifyContent="space-between">
+                          <Button colorScheme="grey" variant="outline">
+                            Assign group
+                          </Button>
+                          <Button colorScheme="grey" variant="outline">
+                            Assign People
+                          </Button>
+                        </Flex>
+                      </Box>
+                    </Flex>
+                  </Box>
+
+                  <Box w="446px" mb="0.4rem">
+                    <Flex>
+                      <Text mt="0.5rem" w="180px">
+                        deepak.onlinework13
+                      </Text>
+                      <Select placeholder="Project Manager" w="280px">
+                        <option>Project Manager</option>
+                        <option>Regular User</option>
+                      </Select>
+                    </Flex>
+                  </Box>
+
+                  <Divider mt="0.4rem"></Divider>
+
+                  <Box w="446px" mb="0.4rem">
+                    <FormControl>
+                      <FormLabel>Description</FormLabel>
+                      <Input type="To add description first save your task" />
+                    </FormControl>
+                  </Box>
+
+                  <Box
+                    w="446px"
+                    // h="20px"
+                    mb="0.8rem"
+                  >
+                    <FormControl>
+                      <FormLabel>Budget</FormLabel>
+                      <Flex justifyContent="space-around">
+                        <Select placeholder="Estimated hours" w="280px">
+                          <option>Estimated hours</option>
+                          <option>Estimated fee</option>
+                        </Select>
+                        <Input
+                          w="160px"
+                          type="To add description first save your task"
+                        />
+                      </Flex>
+                    </FormControl>
+                  </Box>
+
+                  <Box
+                    w="446px"
+                    // h="20px"
+                    mb="0.8rem"
+                  >
+                    <Flex alignItems="flex-start">
+                      <Checkbox defaultChecked>
+                        Time from this task is billable by default
+                      </Checkbox>
+                    </Flex>
+                  </Box>
+
+                  <Divider mb="0.8rem"></Divider>
+
+                  <Box
+                    w="446px"
+                    // h="20px"
+                  >
+                    <FormLabel>Tags</FormLabel>
+                    {/* <Text>Tags</Text> */}
+                  </Box>
+
+                  <Box w="446px" mb="0.8rem">
+                    <Flex justifyContent="space-around">
+                      <Text mt="1.2rem" fontSize="12px">
+                        All tags allowed to be selected when users track time.
+                      </Text>
+                      <Button colorScheme="grey" variant="outline">
+                        Define tag list
+                      </Button>
+                    </Flex>
+                  </Box>
+
+                  <Box
+                    w="446px"
+                    // h="20px"
+                    mb="0.8rem"
+                  >
+                    <Button w="100%" colorScheme="grey" variant="outline">
+                      Enable keywords
+                    </Button>
+                  </Box>
+
+                  <Divider mb="0.8rem"></Divider>
+
+                  <Box
+                    w="446px"
+                    // h="20px"
+                  >
+                    <Flex flexDirection="row" alignItems="flex-start">
+                      <Button colorScheme="whatsapp" variant="solid">
+                        Save
                       </Button>
                       <Button ml="0.8rem" colorScheme="grey" variant="outline">
                         Cancel
