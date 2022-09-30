b7a5196969f00a5fe5e2c2ee9a6c1a333a8313ba
gkomsai <gkomsai788@gmail.com>
2022-09-30 15:33:10 +0530

    empty email login error bug fixed

--- frontend/src/utils/extraFunctions.js
+++ frontend/src/utils/extraFunctions.js
@@ -1,10 +1,10 @@
-export const notify = (toast, title, status, description) => {
+export const notify = (toast, title, status, position="top", description) => {
   return toast({
     title,
     status,
     description,
     duration: 3000,
-    position: "top",
+    position: position,
     isClosable: true,
   });
 };
