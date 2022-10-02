ba28ffce5eb46187245eba24b2c6d84a6518ac63
gkomsai <gkomsai788@gmail.com>
2022-10-02 13:04:55 +0530

    PrivateRoute created and added in the Routes folder

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Projects from "../Pages/Projects";
 import Home from "../Pages/homepage/Home";
 import Pricing from "../Pages/Pricing/Pricing";
@@ -7,14 +7,12 @@ import Signup from "../Pages/Signup/Signup";
 import Sidenav from "../Components/sidenavbar/Sidenav";
 import Login from "../Pages/Login/Login";
 
-
-import Tags from "../Pages/Tags/Tags"
+import Tags from "../Pages/Tags/Tags";
 import Forgotpassword from "../Pages/ForgetPassword/ForgotPassword";
 import ResetPassword from "../Pages/ForgetPassword/ResetPassword";
 import Timesheet from "../Pages/TimeSheet/Timesheet";
 import Error from "../Pages/Error";
-
-
+import PrivateRoute from "../Components/PrivateRoute";
 
 const AllRoutes = () => {
   return (
@@ -24,15 +22,20 @@ const AllRoutes = () => {
 
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/forgotten_password" element={<Forgotpassword/>} />
+      <Route path="/forgotten_password" element={<Forgotpassword />} />
       <Route path="/reset-password/:id/:token" element={<ResetPassword />} />
 
-      <Route path='/app' element={<Sidenav />}>
-
-          <Route path='timesheet' element={<Timesheet />} />
-          <Route path='projects' element={<Projects />} /> 
-          <Route path='tags' element={<Tags />} />
-
+      <Route
+        path="/app"
+        element={
+          <PrivateRoute>
+            <Sidenav />
+          </PrivateRoute>
+        }
+      >
+        <Route path="timesheet" element={<Timesheet />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="tags" element={<Tags />} />
       </Route>
       <Route path="*" element={<Error />} />
     </Routes>
