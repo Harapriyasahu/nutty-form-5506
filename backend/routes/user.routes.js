ffba4b8eecdbf886eec881615a1d536c5fd71a98
gkomsai <gkomsai788@gmail.com>
2022-10-07 10:37:41 +0530

    email Passwordrequired validator added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,91 +7,91 @@ require("dotenv").config();
 const { transporter } = require("../config/emailConfig.js");
 const { emailValidator } = require("../middleware/emailValidator");
 const { passwordValidator } = require("../middleware/PasswordValidator");
+const {
+  emailPassRequiredValidator,
+} = require("../middleware/emailPassRequiredValidator");
 
 const userRouter = Router();
 
-userRouter.post("/signup",[emailValidator,passwordValidator] async (req, res) => {
-  try {
-    console.log(req.body);
-    const { email, password } = req.body;
-    const isEmailPresent = await UserModel.findOne({ email });
-    // console.log(isEmailPresent);
-    if (isEmailPresent) {
-      res
-        .status(400)
-        .send({ status: "error", message: "Email already exists" });
-    } else {
-      bcrypt
-        .hash(password, 10)
-        .then(async function (hash) {
-          const newUser = new UserModel({ ...req.body, password: hash });
-          await newUser.save();
+userRouter.post(
+  "/signup",
+  [emailPassRequiredValidator, emailValidator, passwordValidator],
+  async (req, res) => {
+    try {
+      // console.log(req.body);
+      const { email, password } = req.body;
+      const isEmailPresent = await UserModel.findOne({ email });
+      // console.log(isEmailPresent);
+      if (isEmailPresent) {
+        res
+          .status(400)
+          .send({ status: "error", message: "Email already exists" });
+      } else {
+        bcrypt
+          .hash(password, 10)
+          .then(async function (hash) {
+            const newUser = new UserModel({ ...req.body, password: hash });
+            await newUser.save();
 
-          return res.status(201).send({
-            message: "Signup Sussessfull",
-            status: "success",
-            user: newUser,
-          });
-        })
-        .catch((err) => {
-          // console.log(err);
-
-          return res
-            .status(400)
-            .send({ status: "error", message: err.message });
-        });
-    }
-  } catch (err) {
-    return res
-    .status(400)
-    .send({ status: "error", message: err.message });
-  }
-});
-
-userRouter.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (email && password) {
-      const user = await UserModel.findOne({ email });
-      //   console.log(user);
-      if (user) {
-        let hash = user.password;
-        bcrypt.compare(password, hash, function (err, result) {
-          if (err) {
-            return res.send({
-              message: "Something went wrong, plz try again later",
-              status: "error",
-            });
-          }
-          if (result) {
-            const token = jwt.sign(
-              { userId: user._id, email: email },
-              process.env.JWT_SECRET_KEY,
-              {
-                expiresIn: "5h",
-              }
-            );
-            return res.status(200).send({
+            return res.status(201).send({
+              message: "Signup Sussessfull",
               status: "success",
-              message: "Login Successfull!!!",
-              token: token,
-              user: user,
+              user: newUser,
             });
-          } else {
+          })
+          .catch((err) => {
+            // console.log(err);
+
             return res
               .status(400)
-              .send({ status: "error", message: "Invalid Credentials" });
-          }
-        });
-      } else {
-        return res
-          .status(400)
-          .send({ status: "error", message: "Invalid Credentials" });
+              .send({ status: "error", message: err.message });
+          });
       }
+    } catch (err) {
+      return res.status(400).send({ status: "error", message: err.message });
+    }
+  }
+);
+
+userRouter.post("/login", emailPassRequiredValidator, async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    //   console.log(user);
+    if (user) {
+      let hash = user.password;
+      bcrypt.compare(password, hash, function (err, result) {
+        if (err) {
+          return res.send({
+            message: "Something went wrong, plz try again later",
+            status: "error",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            { userId: user._id, email: email },
+            process.env.JWT_SECRET_KEY,
+            {
+              expiresIn: "5h",
+            }
+          );
+          return res.status(200).send({
+            status: "success",
+            message: "Login Successfull!!!",
+            token: token,
+            user: user,
+          });
+        } else {
+          return res
+            .status(400)
+            .send({ status: "error", message: "Invalid Credentials" });
+        }
+      });
     } else {
       return res
         .status(400)
-        .send({ status: "error", message: "All Fields are Required" });
+        .send({ status: "error", message: "Invalid Credentials" });
     }
   } catch (err) {
     // console.log(err);
@@ -111,10 +111,7 @@ userRouter.post("/forgotten_password", async (req, res) => {
       });
       const link = `https://timecamp-clone-vert.vercel.app/reset-password/${user._id}/${token}`;
 
-
-
-
-      console.log(link);
+      // console.log(link);
 
       let info = await transporter.sendMail({
         from: process.env.EMAIL_FROM,
@@ -131,52 +128,55 @@ userRouter.post("/forgotten_password", async (req, res) => {
     } else {
       res
         .status(400)
-        .send({ status: "failed", message: "Email doesn't exists" });
+        .send({ status: "error", message: "Email doesn't exists" });
     }
   } else {
     res
       .status(400)
-      .send({ status: "failed", message: "Email Field is Required" });
+      .send({ status: "error", message: "Email Field is Required" });
   }
 });
 
-userRouter.post("/reset-password/:id/:token", async (req, res) => {
-  const { password, confirmPassword } = req.body;
-  const {id, token }= req.params;
-  console.log(req.params);
-  console.log(id)
+userRouter.post(
+  "/reset-password/:id/:token",
+  passwordValidator,
+  async (req, res) => {
+    const { password, confirmPassword } = req.body;
+    const { id, token } = req.params;
+    // console.log(req.params);
+    // console.log(id)
 
-  const user = await UserModel.findById(id);
+    const user = await UserModel.findById(id);
 
-  try {
-    jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if (password && confirmPassword) {
-      if (password !== confirmPassword) {
-        return res
-          .status(500)
-          .send({
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_KEY);
+      if (password && confirmPassword) {
+        if (password !== confirmPassword) {
+          return res.status(500).send({
             status: "error",
             message: "New Password and Confirm New Password doesn't match",
           });
+        } else {
+          const salt = await bcrypt.genSalt(10);
+          const newHashPassword = await bcrypt.hash(password, salt);
+          await UserModel.findByIdAndUpdate(user._id, {
+            $set: { password: newHashPassword },
+          });
+          res.status(201).send({
+            status: "success",
+            message: "Password Reset Successfully",
+          });
+        }
       } else {
-        const salt = await bcrypt.genSalt(10);
-        const newHashPassword = await bcrypt.hash(password, salt);
-        await UserModel.findByIdAndUpdate(user._id, {
-          $set: { password: newHashPassword },
-        });
         res
-          .status(201)
-          .send({ status: "success", message: "Password Reset Successfully" });
+          .status(400)
+          .send({ status: "error", message: "All Fields are Required" });
       }
-    } else {
-      res
-        .status(400)
-        .send({ status: "error", message: "All Fields are Required" });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({ status: "error", message: "Invalid Token" });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({ status: "error", message: "Invalid Token" });
   }
-});
+);
 
 module.exports = { userRouter };
